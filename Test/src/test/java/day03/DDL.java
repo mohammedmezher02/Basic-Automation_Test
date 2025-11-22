d976e9fbc7f256a4c801c2389e9dc0f32c13d426
mohammedmezher02 <mezhermohammed72@gmail.com>
2025-11-23 01:16:43 +0300

    new edit

--- Test/src/test/java/day03/DDL.java
+++ Test/src/test/java/day03/DDL.java
@@ -6,37 +6,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DDL {
-	ChromeDriver driver=new ChromeDriver();
-	
-	@BeforeMethod
-	public void Test(){
-		WebDriverManager.chromedriver().setup();
-		driver.get("https://www.hyrtutorials.com/p/html-dropdown-elements-practice.html");
-		driver.manage().window().maximize();
-		
-	}
-  @org.testng.annotations.Test
-public void TestCase1(){
-	WebElement hh = driver.findElement(By.id("ide"));
-	
-	Select multi=new Select(hh);
-	//by index ,by visible text 
-	multi.selectByValue("ij");
-	multi.selectByValue("vs");
-	multi.selectByValue("nb");
-	
-	multi.deselectByValue("nb");
-	}
- @AfterMethod	
- 
- public void Exit() {
-	 
-	 driver.quit();
- }
-	}
+    ChromeDriver driver;
 
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.hyrtutorials.com/p/html-dropdown-elements-practice.html");
+        driver.manage().window().maximize();
+    }
 
+    @Test
+    public void TestCase1() {
+        WebElement hh = driver.findElement(By.id("ide"));
+        Select multi = new Select(hh);
+
+        multi.selectByValue("ij");
+        multi.selectByValue("vs");
+        multi.selectByValue("nb");
+
+        multi.deselectByValue("nb");
+    }
+
+    @AfterMethod
+    public void Exit() {
+        driver.quit();
+    }
+}
